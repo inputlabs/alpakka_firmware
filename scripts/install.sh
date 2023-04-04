1784e5e25d1a1d1769d525c8e6279096c03ba1f3
Marcos Diaz <marcos@inputlabs.io>
2023-04-04 19:29:48 +0300

    Use vars from common file

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,20 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (C) 2022, Input Labs Oy.
 
+# Pico SDK.
 SDK_URL=https://github.com/raspberrypi/pico-sdk.git
 SDK_TAG=1.3.1
 
+# ARM toolchain.
 # WEBSITE: https://developer.arm.com/downloads/-/gnu-rm
 ARM_URL_COMMON=https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10
 ARM_FILENAME_DARWIN=gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2
 ARM_FILENAME_LINUX=gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 ARM_TAR=arm-toolchain.tar.bz2
 ARM_DIR=arm-toolchain
+
+# Do not remove this.
+## GITHUB IMPORTS ENVS UNTIL HERE
 
 rm -rf deps
 mkdir deps
@@ -27,7 +32,7 @@ curl --progress-bar -L -o $ARM_TAR $ARM_URL
 
 echo 'Extracting ARM toolchain...'
 mkdir $ARM_DIR
-tar -xvf $ARM_TAR --directory $ARM_DIR --strip-components 1
+tar -xf $ARM_TAR --directory $ARM_DIR --strip-components 1
 rm $ARM_TAR
 
 echo "Downloading Pico C SDK..."
