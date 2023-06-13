374c5b99a8ce4cfd0a65baee159ff6e1d6481da2
Marcos Diaz <marcos@inputlabs.io>
2023-06-13 14:25:18 +0300

    Fixed helper macros without encapsulation

--- src/headers/helper.h
+++ src/headers/helper.h
@@ -8,13 +8,13 @@
 #define min(a, b)  ((a < b) ? a : b)
 #define max(a, b)  ((a > b) ? a : b)
 #define limit_between(value, low, high)  max(low, min(high, value))
-#define is_between(value, low, high)  ((value >= low) && (value <= high))
-#define degrees(radians)  (radians * 180.0 / M_PI)
-#define radians(degrees)  (degrees * M_PI / 180.0)
-#define ramp_low(x, z)  x<z ? 0 : (x-z) * (1 / (1-z))
-#define ramp_inv(x, z, max)  x + (z*(max-x))
-#define ramp_mid(x, z)  x<z ? 0 : x>(1-z) ? 1 : (x-z) * (1 / (1 - 2*z))
-#define ramp(x, min, max)  x<=min ? 0 : (x>=max ? 1 : (1 * (x-min) / (max-min)))
+#define is_between(value, low, high)  ( (value >= low) && (value <= high) )
+#define degrees(radians)  ( radians * 180.0 / M_PI )
+#define radians(degrees)  ( degrees * M_PI / 180.0 )
+#define ramp_low(x, z)  ( x<z ? 0 : (x-z) * (1 / (1-z)) )
+#define ramp_inv(x, z, max)  ( x + (z*(max-x)) )
+#define ramp_mid(x, z)  ( x<z ? 0 : x>(1-z) ? 1 : (x-z) * (1 / (1 - 2*z)) )
+#define ramp(x, min, max)  ( x<=min ? 0 : (x>=max ? 1 : (1 * (x-min) / (max-min))) )
 
 uint32_t bin(uint8_t k);
 uint32_t bin16(uint16_t k);
