6347ba13f73cc2caffd28632ec8c0f93c5f6b268
Marcos Diaz <marcos@inputlabs.io>
2023-06-18 20:01:49 +0300

    Additional mappings

--- src/profiles/racing.c
+++ src/profiles/racing.c
@@ -10,9 +10,9 @@ Profile profile_init_racing() {
     Profile profile = Profile_();
 
     profile.select_1 = Button_(PIN_SELECT_1, NORMAL, ACTIONS(GAMEPAD_SELECT));
-    profile.select_2 = Button_(PIN_SELECT_2, NORMAL, ACTIONS(KEY_NONE));
+    profile.select_2 = Button_(PIN_SELECT_2, NORMAL, ACTIONS(KEY_M));
     profile.start_1 =  Button_(PIN_START_1,  NORMAL, ACTIONS(GAMEPAD_START));
-    profile.start_2 =  Button_(PIN_START_2,  NORMAL, ACTIONS(KEY_NONE));
+    profile.start_2 =  Button_(PIN_START_2,  NORMAL, ACTIONS(KEY_N));
 
     profile.dpad_left =  Button_(PIN_DPAD_LEFT,  NORMAL, ACTIONS(GAMEPAD_LEFT));
     profile.dpad_right = Button_(PIN_DPAD_RIGHT, NORMAL, ACTIONS(GAMEPAD_RIGHT));
@@ -31,11 +31,12 @@ Profile profile_init_racing() {
     profile.l4 = Button_(PIN_L4, NORMAL, ACTIONS(GAMEPAD_LEFT));
     profile.r4 = Button_(PIN_R4, NORMAL, ACTIONS(GAMEPAD_RIGHT));
 
-    profile.rotary = Rotary_(NULL, ACTIONS(KEY_PAD_1), ACTIONS(KEY_PAD_2));
-    profile.rotary.config_mode(&profile.rotary, 1, ACTIONS(KEY_PAD_3), ACTIONS(KEY_PAD_4));
-    profile.rotary.config_mode(&profile.rotary, 2, ACTIONS(KEY_PAD_5), ACTIONS(KEY_PAD_6));
-    profile.rotary.config_mode(&profile.rotary, 3, ACTIONS(KEY_PAD_7), ACTIONS(KEY_PAD_8));
-    profile.rotary.config_mode(&profile.rotary, 4, ACTIONS(PROC_ADZ), ACTIONS(PROC_ADZN));
+    profile.rotary = Rotary_(NULL, ACTIONS(KEY_UP), ACTIONS(KEY_DOWN));
+    profile.rotary.config_mode(&profile.rotary, 1, ACTIONS(KEY_PAD_1), ACTIONS(KEY_PAD_2));
+    profile.rotary.config_mode(&profile.rotary, 2, ACTIONS(KEY_PAD_3), ACTIONS(KEY_PAD_4));
+    profile.rotary.config_mode(&profile.rotary, 3, ACTIONS(KEY_PAD_5), ACTIONS(KEY_PAD_6));
+    profile.rotary.config_mode(&profile.rotary, 4, ACTIONS(KEY_PAD_7), ACTIONS(KEY_PAD_8));
+    profile.rotary.config_mode(&profile.rotary, 5, ACTIONS(PROC_ADZ), ACTIONS(PROC_ADZN));
 
     profile.thumbstick = Thumbstick_(
         THUMBSTICK_MODE_RADIAL,                                  // Mode.
@@ -54,49 +55,57 @@ Profile profile_init_racing() {
         // Left.
         Button_(
             PIN_VIRTUAL,
-            HOLD_OVERLAP,
-            ACTIONS(GAMEPAD_AXIS_RX_NEG, PROC_ROTARY_MODE_0),
-            ACTIONS(PROC_ROTARY_MODE_4)
+            NORMAL,
+            ACTIONS(GAMEPAD_AXIS_RX_NEG, PROC_ROTARY_MODE_1)
         ),
         // Right.
-        Button_(PIN_VIRTUAL,
+        Button_(
+            PIN_VIRTUAL,
             NORMAL,
-            ACTIONS(GAMEPAD_AXIS_RX, PROC_ROTARY_MODE_2)
+            ACTIONS(GAMEPAD_AXIS_RX, PROC_ROTARY_MODE_3)
         ),
         // Up.
-        Button_(PIN_VIRTUAL,
+        Button_(
+            PIN_VIRTUAL,
             NORMAL,
-            ACTIONS(GAMEPAD_AXIS_RY_NEG, PROC_ROTARY_MODE_3)
+            ACTIONS(GAMEPAD_AXIS_RY_NEG, PROC_ROTARY_MODE_4)
         ),
         // Down.
-        Button_(PIN_VIRTUAL,
+        Button_(
+            PIN_VIRTUAL,
             NORMAL,
-            ACTIONS(GAMEPAD_AXIS_RY, PROC_ROTARY_MODE_1)
+            ACTIONS(GAMEPAD_AXIS_RY, PROC_ROTARY_MODE_2)
         ),
         // ↖
-        Button_(PIN_VIRTUAL,
+        Button_(
+            PIN_VIRTUAL,
             NORMAL,
             ACTIONS(GAMEPAD_AXIS_RX_NEG, GAMEPAD_AXIS_RY_NEG)
         ),
         // ↗
-        Button_(PIN_VIRTUAL,
+        Button_(
+            PIN_VIRTUAL,
             NORMAL,
             ACTIONS(GAMEPAD_AXIS_RX, GAMEPAD_AXIS_RY_NEG)
         ),
         // ↙
-        Button_(PIN_VIRTUAL,
+        Button_(
+            PIN_VIRTUAL,
             NORMAL,
             ACTIONS(GAMEPAD_AXIS_RX_NEG, GAMEPAD_AXIS_RY)
         ),
         // ↘
-        Button_(PIN_VIRTUAL,
+        Button_(
+            PIN_VIRTUAL,
             NORMAL,
             ACTIONS(GAMEPAD_AXIS_RX, GAMEPAD_AXIS_RY)
         ),
         // Push.
-        Button_(PIN_VIRTUAL,
-            NORMAL,
-            ACTIONS(GAMEPAD_DOWN)
+        Button_(
+            PIN_VIRTUAL,
+            HOLD_OVERLAP,
+            ACTIONS(GAMEPAD_DOWN, PROC_ROTARY_MODE_0),
+            ACTIONS(PROC_ROTARY_MODE_5)
         )
     );
 
