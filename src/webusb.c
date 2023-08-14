de0edba3cdcfaf6de197e4235fb0d0db125cbc5b
Marcos Diaz <marcos@inputlabs.io>
2023-08-14 21:53:30 +0300

    Simpler buffer manipulation

--- src/webusb.c
+++ src/webusb.c
@@ -14,21 +14,20 @@ char webusb_buffer[1024] = {0,};
 uint16_t webusb_ptr_in = 0;
 uint16_t webusb_ptr_out = 0;
 bool webusb_timedout = false;
-bool webusb_buffer_lock = false;
 
 void webusb_flush_force() {
     uint16_t i = 0;
     while(true) {
-        busy_wait_ms(1);
         tud_task();
         bool sent = webusb_flush();
         if (sent) {
             break;
         }
         else {
+            sleep_ms(1);
             i++;
             if (i>500) {
-                printf("USB: WebUSB not found\n");
+                printf("USB: WebUSB timed out\n");
                 webusb_timedout = true;
                 return;
             }
@@ -40,17 +39,16 @@ bool webusb_flush() {
     if (webusb_timedout) return true;
     if (webusb_ptr_in == 0) return true;
     if (!tud_ready() || usbd_edpt_busy(0, ADDR_WEBUSB_IN)) return false;
-    char part[64] = {0,};
     uint16_t len = max(0, min(64, webusb_ptr_in - webusb_ptr_out));
-    strncpy(part, webusb_buffer + webusb_ptr_out, len);
+    uint8_t* offset_ptr = webusb_buffer + webusb_ptr_out;
+    usbd_edpt_claim(0, ADDR_WEBUSB_IN);
+    usbd_edpt_xfer(0, ADDR_WEBUSB_IN, offset_ptr, len);
+    usbd_edpt_release(0, ADDR_WEBUSB_IN);
     webusb_ptr_out += len;
     if (webusb_ptr_out >= webusb_ptr_in) {
         webusb_ptr_in = 0;
         webusb_ptr_out = 0;
     }
-    usbd_edpt_claim(0, ADDR_WEBUSB_IN);
-    usbd_edpt_xfer(0, ADDR_WEBUSB_IN, (uint8_t*)part, len);
-    usbd_edpt_release(0, ADDR_WEBUSB_IN);
     return true;
 }
 
@@ -61,7 +59,7 @@ void webusb_write(char *msg) {
         printf("Warning: Full WebUSB buffer\n");
         return;
     }
-    strncpy(webusb_buffer+webusb_ptr_in, msg, len);
+    strncpy(webusb_buffer + webusb_ptr_in, msg, len);
     webusb_ptr_in += len;
     if (!logging_get_onloop()) {
         webusb_flush_force();
