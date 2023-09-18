ff1d991b3d742e94f48076b18767430d98c2af6b
Marcos Diaz <marcos@inputlabs.io>
2023-09-18 20:08:26 +0300

    Fixed logging tagged binary as hexadecimal

--- src/imu.c
+++ src/imu.c
@@ -40,7 +40,7 @@ void imu_init_single(uint8_t cs, uint8_t gyro_conf) {
     bus_spi_write(cs, IMU_CTRL2_G, gyro_conf);
     uint8_t xl = bus_spi_read_one(cs, IMU_CTRL1_XL);
     uint8_t g = bus_spi_read_one(cs, IMU_CTRL2_G);
-    info("  IMU cs=%i id=0x%02x xl=0x%08i g=0x%08i\n", cs, id, bin(xl), bin(g));
+    info("  IMU cs=%i id=0x%02x xl=0b%08i g=0b%08i\n", cs, id, bin(xl), bin(g));
 }
 
 void imu_init() {
